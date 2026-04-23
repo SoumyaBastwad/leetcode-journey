6883d0256848fddc36ff0700b2a6cec57f6c0495
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-23 23:36:29 +0530

    Update Fibonaccinumber.py

--- Fibonaccinumber.py
+++ Fibonaccinumber.py
@@ -1,21 +1,5 @@
 # the Fibonacci sequence:such that each number is the sum of the two preceding ones, starting from 0 and 1.
-def fib(n):
-    a=0
-    b=1
-    if n<=0:
-        print("invalid number")
-    elif n==1:
-        print(a)
-    else:
-        print(a)
-        print(b)
-        for i in range(2,n):
-            c=a+b
-            a=b
-            b=c
-            print(c)
-n=int(input("enter a integer:"))
-fib(n)
+
 
 
 
