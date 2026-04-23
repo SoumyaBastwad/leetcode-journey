1b400080aaa89395192be19df02a2d64a8317267
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-23 23:37:57 +0530

    Update PowerOf2.py

--- PowerOf2.py
+++ PowerOf2.py
@@ -1,9 +1,3 @@
 # ispowerof2 
 # in that input taken as integer n and if that n is a power of 2 it's showing true
 #  if n is not a power of 2 it's showing False
-n=int(input("Enter the n value a integer:"))
-if n<=0:
-     print(False)        # n is negative or o 
-while n%2==0:
-    n=n//2
-print(n==1)             # if n is power of 2
