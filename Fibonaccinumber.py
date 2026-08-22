dcd1ec295418e65f88cf5e2acecb7d3058e1be03
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-22 22:11:38 +0530

    Update Fibonaccinumber.py

--- Fibonaccinumber.py
+++ Fibonaccinumber.py
@@ -8,14 +8,7 @@ def fib(n):
         print(a)
     else:
         print(a)
-        print(b)
-        for i in range(2,n):
-            c=a+b
-            a=b
-            b=c
-            print(c)
-n=int(input("enter a integer:"))
-fib(n)
+     
 
 
 
