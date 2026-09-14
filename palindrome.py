f62f8a2ea37c0d67ba95a0b9a8210f6dc6bc6759
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-14 19:55:48 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,8 +3,3 @@
 x=input("enter a integer number")
 rev=""
 for i in x:
-    rev=i+rev
-if rev==x:
-    print("the given number is palindrome")
-else:
-    print("the given number is not a palindrome")
