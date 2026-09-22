903edbef09d731052afc69e807afe88c4fdeaf35
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-22 23:53:35 +0530

    Update Fibonaccinumber.py

--- Fibonaccinumber.py
+++ Fibonaccinumber.py
@@ -4,16 +4,4 @@ def fib(n):
     b=1
     if n<=0:
         print("invalid number")
-    elif n==1:
-        print(a)
-    else:
-        print(a)
-        print(b)
-        for i in range(2,n):
-            c=a+b
-            a=b
-            b=c
-            print(c)
-n=int(input("enter a integer:"))
-fib(n)
-
+    elif n==
